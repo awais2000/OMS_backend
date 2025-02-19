4adfdff3cc4f8abf083139af5836dc3fc93fcb76
awais2000 <135193628+awais2000@users.noreply.github.com>
2025-02-19 16:07:01 +0500

    Update adminRoutes.ts

--- backend/routes/adminRoutes.ts
+++ backend/routes/adminRoutes.ts
@@ -9,36 +9,38 @@ export default (app: Application): void => {
 
     app.get("/admin/home", authenticateToken, isAdmin, getAllUsers);
 
-    // app.post('/admin/addUser',upload.single("document"), authenticateToken, isAdmin, addUser);
-    app.post('/admin/addUser',upload.single("document"),  addUser);
+    app.post('/admin/addUser',upload.single("document"), authenticateToken, isAdmin, addUser);
+    // app.post('/admin/addUser',upload.single("document"),  addUser);
 
-    // app.put('/admin/updateUser/:id', authenticateToken, isAdmin, upload.single("document"), updateUser);
-    app.put('/admin/updateUser/:id',   upload.single("document"), updateUser);
+    app.put('/admin/updateUser/:id', authenticateToken, isAdmin, upload.single("document"), updateUser);
+    // app.put('/admin/updateUser/:id',   upload.single("document"), updateUser);
 
-    // app.delete("/admin/deleteUser/:id", authenticateToken, isAdmin, deleteUser);
-    app.delete("/admin/deleteUser/:id",  deleteUser);
+    app.delete("/admin/deleteUser/:id", authenticateToken, isAdmin, deleteUser);
+    // app.delete("/admin/deleteUser/:id",  deleteUser);
 
     //adding Customer Routes:
-    // app.get('/admin/getAllCust', authenticateToken, isAdmin, getAllCust);
-    app.get('/admin/getAllCust', getAllCust);
+    app.get('/admin/getAllCust', authenticateToken, isAdmin, getAllCust);
+    // app.get('/admin/getAllCust', getAllCust);
 
-    // app.post('/admin/addCustInfo', authenticateToken, isAdmin, addCustInfo);
-    app.post('/admin/addCustInfo', addCustInfo);
+    app.post('/admin/addCustInfo', authenticateToken, isAdmin, addCustInfo);
+    // app.post('/admin/addCustInfo', addCustInfo);
 
     app.put('/admin/updateCust/:id', authenticateToken, isAdmin, updateCust);
 
     app.delete('/admin/deleteCust/:id', authenticateToken, isAdmin, deleteCust);
 
     //for attendance
-    // app.get('/admin/getAttendance', authenticateToken, isAdmin, getAttendance);
-    app.get('/admin/getAttendance', getAttendance);
+    app.get('/admin/getAttendance', authenticateToken, isAdmin, getAttendance);
+    // app.get('/admin/getAttendance', getAttendance);
 
-    // app.post('/admin/addAttendance', authenticateToken, isAdmin, updateAttendance);
-    app.post('/admin/addAttendance', addAttendance);
+    app.post('/admin/addAttendance', authenticateToken, isAdmin, updateAttendance);
+    // app.post('/admin/addAttendance', addAttendance);
 
-    // app.put('/admin/updateAttendance', authenticateToken, isAdmin, updateAttendance);
-    app.put('/admin/updateAttendance/:id', updateAttendance);
+    app.put('/admin/updateAttendance', authenticateToken, isAdmin, updateAttendance);
+    // app.put('/admin/updateAttendance/:id', updateAttendance);
 
-    // app.delete('/admin/deleteAttendance', authenticateToken, isAdmin, deleteAttendance);
-    app.patch('/admin/deleteAttendance/:id', deleteAttendance);
+    app.delete('/admin/deleteAttendance', authenticateToken, isAdmin, deleteAttendance);
+    // app.patch('/admin/deleteAttendance/:id', deleteAttendance);
 };
+
+//sin1::tgsh7-1739958814948-5e6e7d0b934e
