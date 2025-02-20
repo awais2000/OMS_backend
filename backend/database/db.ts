8d521e8ad6b1a3897e0d1ad13ff76f8aa602da4b
awais2000 <135193628+awais2000@users.noreply.github.com>
2025-02-20 15:50:09 +0500

    kjnin

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST || "127.0.0.1",
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_NAME || "oms",
