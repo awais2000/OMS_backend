d290ec1e76738967f28a6bad27a03ecf28ffd3d7
awais2000 <135193628+awais2000@users.noreply.github.com>
2025-08-02 00:18:58 -0700

    Update

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -5,19 +5,10 @@ dotenv.config();
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "oms",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
-
-
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: process.env.DB_HOST || "147.79.64.150",
-//   user: process.env.DB_USER || "u334339390_uSRomsSyStEM",
-//   password: process.env.DB_PASSWORD || "PSsuSRomsSyStEM!3",
-//   database: process.env.DB_NAME || "u334339390_oFFicEmnGTSyS",
-// });
 
 export default pool; 
